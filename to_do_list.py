3ed845b0e3c6982791ac70c2a6d3b0870badda4f
Daniel Pye <danielpye@zoho.com>
2017-04-04 18:28:46 +0700

    Updates to do list app

--- to_do_list.py
+++ to_do_list.py
@@ -4,6 +4,17 @@ to_do_list = []
 # print out instructions on how to use the app
 print("What do you need to do today?")
 print("Enter 'DONE' to stop adding items to your to do list.")
+print("Enter 'SHOW' to see what's already on the list or 'HELP' for a list of commands.")
+
+# define the help function
+def help():
+    print("To exit the app, enter 'DONE'. To view items already on your list, enter 'SHOW'.")
+
+# display current list if user enters SHOW command
+def show():
+    print("Here's your list so far: ")
+    for item in to_do_list:
+        print(item)
 
 while True:
     #ask for new items
@@ -11,6 +22,12 @@ while True:
     # enter the word DONE to quit the app
     if new_item == 'DONE':
         break
+    elif new_item == 'HELP':
+        help()
+        continue
+    elif new_item == 'SHOW':
+        show()
+        continue
     # add new item to the list
     else:
         to_do_list.append(new_item)
