1f2d054d41a887162a44447f1dd5fdc3d56db18f
Daniel Pye <danielpye@zoho.com>
2017-04-05 20:23:26 +0700

    Adds letter_game.py

---
+++ letter_game.py
@@ -0,0 +1,100 @@
+import random
+import os
+import sys
+
+# make a list of words
+words = [
+    'apple',
+    'bannana',
+    'orange',
+    'coconut',
+    'lemon',
+    'melon',
+    'blueberry',
+    'rasberry',
+    'lime',
+    'grapefruit'
+]
+
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def draw(bad_guesses, good_guesses, secret_word):
+    clear()
+    print("Strikes: {}/7".format(len(bad_guesses)))
+    print('')
+
+    for letter in bad_guesses:
+        print(letter, end='')
+    print("\n\n")
+
+    #draw guessed letters, spaces, and strikes
+    for letter in secret_word:
+        if letter in good_guesses:
+            print(letter, end='')
+        else:
+            print('_', end='')
+    print('')
+
+def get_guess(bad_guesses, good_guesses):
+    #take a guess
+    while True:
+        guess = input("Guess a letter: ").lower()
+        if len(guess) != 1:
+            print("You can only guess a single letter at a time.")
+        elif guess in bad_guesses or guess in good_guesses:
+            print("You've already guessed that letter.")
+        elif not guess.isalpha():
+            print("You can only guess letters.")
+        else:
+            return guess
+
+def play(done):
+    clear()
+    secret_word = random.choice(words)
+    bad_guesses = []
+    good_guesses = []
+
+    while True:
+        draw(bad_guesses, good_guesses, secret_word)
+        guess = get_guess(bad_guesses, good_guesses)
+
+        if guess in secret_word:
+            good_guesses.append(guess)
+            found = True
+            for letter in secret_word:
+                if letter not in good_guesses:
+                    found = False
+            if found:
+                print("You win!\nThe secret word was {}.".format(secret_word))
+                done = True
+        else:
+            bad_guesses.append(guess)
+            if len(bad_guesses) == 7:
+                draw(bad_guesses, good_guesses, secret_word)
+                print("You lost.\nThe secret word was {}.".format(secret_word))
+                done = True
+
+        if done:
+            play_again = input("Play again? y/n ").lower()
+            if play_again != 'n':
+                return play(done=False)
+            else:
+                print("Thanks for playing!")
+                sys.exit()
+
+def welcome():
+    print("Welcome to my letter guessing game!\n")
+    start = input("Press enter/return to start or Q to quit ").lower()
+    if start == 'q':
+        print("Bye!")
+        sys.exit
+
+done = False
+while True:
+    clear()
+    welcome()
+    play(done)
