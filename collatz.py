b7401de087d42c8eaea8765aa7e8f8f5d9ceb97b
Daniel Pye <danielpye@zoho.com>
2017-04-12 19:53:35 +0700

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,13 @@
+def collatz(num):
+
+        if num % 2 == 0:
+            print(num // 2)
+            return num // 2
+        elif num % 2 == 1:
+            result = 3 * num + 1
+            print(result)
+            return result
+
+output = input("Enter the number to Collatz:\n")
+while output != 1:
+    output = collatz(int(output))
